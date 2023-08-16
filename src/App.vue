18f16806d4d10fa3fa27a41a32bd8f7f5b5d6710
anigandini <anigandini@gmail.com>
2023-08-16 15:55:23 -0300

    project set up & header

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header></Header>
+</template>
+
+<style>
+:root {
+  --primary: rgba(68, 54, 253, 1);
+  --secondary: rgba(115, 103, 240, 1);
+  --bg: rgba(248, 248, 248, 1);
+  --bg-light: rgba(255, 255, 255, 1);
+  --gray-s: rgba(110, 107, 123, 1);
+  --gray-m: rgba(238, 238, 238, 1);
+  --green: rgba(40, 199, 111, 1);
+  }
+  #app {
+    padding: 0 !important;
+    margin: 0 !important;
+    box-sizing: border-box;
+
+  }
+  body {
+    margin: 0;
+    max-width: 100%;
+    overflow-x: hidden;
+    font-family: 'Montserrat', sans-serif;
+    line-height: 14.63px;
+  }
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+  import LogoEdMachina from './icons/LogoEdMachina.vue'
+  import Language from './icons/IconLanguage.vue'
+  import DarkMode from './icons/IconDarkMode.vue'
+  import Notifications from './icons/IconNotifications.vue'
+</script>
+
+<template>
+  <header>
+    <LogoEdMachina class="header__logo"></LogoEdMachina>
+    <section>
+      <div class="header__options">
+        <Language></Language>
+        <DarkMode></DarkMode>
+        <Notifications></Notifications>
+      </div>
+      <div class="header__info">
+        <div>
+          <span>Hellen Smith</span>
+          <span>Admin</span>
+        </div>
+        <figure>
+          <img src="../assets/adminavatar.png">
+          <div></div>
+        </figure>
+      </div>
+    </section>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+    background-color: var(--primary);
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.75rem;
+    font-size: 0.5rem;
+    color: var(--bg-light);
+  }
+
+  .header__logo {
+    transform: scale(0.7);
+  }
+  section, .header__info, .header__options {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .header__info div {
+    display: flex;
+    flex-direction: column;
+    font-weight: 700;
+    text-align: end;
+  }
+  .header__info figure {
+    margin: 0;
+  }
+
+  .header__info img {
+    border-radius: 50%;
+    border: 1.5px solid var(--secondary);
+    position: relative;
+    top: 0.5rem;
+  }
+
+  .header__info figure div {
+    border-radius: 50%;
+    width: 8px;
+    height: 8px;
+    background-color: var(--green);
+    position: relative;
+    bottom: 0.5rem;
+    left: 2rem;
+
+  }
+
+  @media (min-width: 768px) {
+    header {
+      padding: 0.75rem 1.375rem;
+      font-size: 0.75rem;
+    }
+
+    .header__logo {
+      transform: none;
+    }
+
+    section, .header__info, .header__options {
+      gap: 1rem;
+    }
+  }
+
+
+</style>
+
